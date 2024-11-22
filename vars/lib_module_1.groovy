327eb23d78ccbeafdebabd0bc81c45675d6892f7
Yash Bhardwaj <yash82206@gmail.com>
2024-11-22 16:43:49 +0530

    Create lib_module_1.groovy

---
+++ vars/lib_module_1.groovy
@@ -0,0 +1,4 @@
+def function_from_module_1(){
+  println("This is a test function from module 1 and function function_from_module_1")
+  println("This function can be used in any pipeline stages")
+}
